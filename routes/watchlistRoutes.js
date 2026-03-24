b952d16d717decfc6a3f7d53b590bc2343615711
Nashira1322 <tanjieyu.jessiechen@gmail.com>
2026-03-24 21:04:47 +0800

    updates

--- routes/watchlistRoutes.js
+++ routes/watchlistRoutes.js
@@ -4,11 +4,11 @@ const watchlistController = require('../controllers/watchlistController');
 
 
 router.get('/', watchlistController.list);
-router.get('/search', watchlistController.search);
 router.get('/new', watchlistController.showNew);
+router.get('/search', watchlistController.search);
+router.get('/check', watchlistController.checkInWatchlist);
 router.post('/', watchlistController.create);
 router.get('/:id/edit', watchlistController.showEdit);
-router.get('/check', watchlistController.checkInWatchlist);
 router.post('/:id', watchlistController.update);
 router.post('/:id/delete', watchlistController.remove);
 
